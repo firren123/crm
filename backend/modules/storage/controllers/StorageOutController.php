d4d94435761cafbb579183852798a827dbf0cc2d
孙松松 <sunsongsong@iyangpin.com>
2015-08-28 11:46:21 +0800

    出库控制器

--- backend/modules/storage/controllers/StorageOutController.php
+++ backend/modules/storage/controllers/StorageOutController.php
@@ -54,6 +54,7 @@ class StorageOutController extends BaseController
             $data['status'] = RequestHelper::post('reason', 0, 'intval');
             $data['code'] = $this->gainId();//获取订单编号
             $data['admin_id'] = $this->admin_id;
+            $data['storage_sn'] = RequestHelper::post('depots', '', 'trim');
             $data['remark'] = RequestHelper::post('explain', '', 'trim');
 
             $info['info_id'] = RequestHelper::post('info_id', '', 'trim');
@@ -63,6 +64,7 @@ class StorageOutController extends BaseController
             $info['info_num'] = RequestHelper::post('info_num', '', 'trim');
             $info['info_sum'] = RequestHelper::post('info_sum', '', 'trim');
             $info['info_bar_code'] = RequestHelper::post('info_bar_code', '', 'trim');
+            $info['info_storage_sn'] = RequestHelper::post('info_storage_sn', '', 'trim');
             $info['info_remark'] = RequestHelper::post('info_remark', '', 'trim');
 
             if ($data['status'] == 0) {
@@ -72,7 +74,6 @@ class StorageOutController extends BaseController
             if (empty($data['remark'])) {
                 return $this->error('出库说明不能为空！！！', '/storage/warehouse/index');
             }
-
             if (empty($info)) {
                 return $this->error('出库商品不能为空！！！', '/storage/warehouse/index');
             }
@@ -85,6 +86,7 @@ class StorageOutController extends BaseController
                 $info_num = explode(",", $info['info_num']);
                 $info_sum = explode(",", $info['info_sum']);
                 $info_bar_code = explode(",", $info['info_bar_code']);
+                $info_storage_sn = explode(",", $info['info_storage_sn']);
                 $info_remark = explode(",", $info['info_remark']);
 
                 $info_data = array();
@@ -95,6 +97,7 @@ class StorageOutController extends BaseController
                     $info_data[$k]['price'] = $info_pric[$k];
                     $info_data[$k]['num'] = $info_num[$k];
                     $info_data[$k]['bar_code'] = $info_bar_code[$k];
+                    $info_data[$k]['storage_sn'] = $info_storage_sn[$k];
                     $info_data[$k]['sum_price'] = $info_sum[$k];
                     $info_data[$k]['remark'] = $info_remark[$k];
                 }
@@ -109,13 +112,13 @@ class StorageOutController extends BaseController
                             $v['out_id'] = $storage_id;
                             $storage_good->insertInfo($v);
                             //日志
-                            $goods = "商品出库关联商品>商品ID:".$v['good_id'].",关联出库单ID:".$v['out_id'].",商品名称:".$v['good_name'].",商品单价:".$v['price'].",商品数量:".$v['num'];
+                            $goods = "商品出库关联商品>商品ID:".$v['good_id'].",关联出库单ID:".$v['out_id'].",商品名称:".$v['good_name'].",商品单价:".$v['price'].",商品数量:".$v['num'].",仓库编号:".$v['storage_sn'];
                             $log_model->recordLog($goods, 8);
                         }
                     }
 
                     //日志
-                    $content = "出库订单表>出库原因:".$data['status'].",出库订单编号：".$data['code'].",出库说明:".$data['remark'];
+                    $content = "出库订单表>出库原因:".$data['status'].",出库订单编号：".$data['code'].",出库说明:".$data['remark'].",仓库编号:".$data['storage_sn'];
                     $log_model->recordLog($content, 8);
 
                     return $this->success('提交数据成功！！！', '/storage/storage-out/list');
@@ -176,14 +179,18 @@ class StorageOutController extends BaseController
     {
         $this->layout = 'dialog';
         $page = RequestHelper::get('page', 1, 'intval');
+        $sn = RequestHelper::get('sn', '', 'trim');
         $page_size = 5;
         $name = RequestHelper::get('name', '', 'trim');
         $where = ['and', ['>', 'id', 0], ['!=', 'good_name', '']];
         if (!empty($name)) {
             $where[] = ['like', 'good_name', $name];
         }
+        if (!empty($sn)) {
+            $where[] = ['=', 'storage_sn', $sn];
+        }
         $goods = new SupplierGood();
-        $files = "id,good_id,sum(num) as total,good_name,attr_value,bar_code";
+        $files = "id,good_id,sum(num) as total,good_name,attr_value,bar_code,storage_sn";
         $list = $goods->getPage($where, $files, 'good_id', 'id desc', $page, $page_size);
         if (empty($list)) {
             $list = array();
@@ -217,6 +224,7 @@ class StorageOutController extends BaseController
         $page = RequestHelper::get('page', 1, 'intval');
         $page_size = 10;
         $storage = new StorageOut();
+        $ware = new Warehouse();
 
         $time_one = RequestHelper::get('time_one', '', 'trim');
         $time_two = RequestHelper::get('time_two', '', 'trim');
@@ -232,13 +240,24 @@ class StorageOutController extends BaseController
         if (!empty($time_two)) {
             $where[] = ['<=', 'create_time', $time_two];
         }
-        $files = "id,code,create_time,remark,status";
+        $files = "id,code,create_time,remark,status,storage_sn";
         $list = $storage->getPageList($where, $files, 'id desc', $page, $page_size);
         $count = $storage->getCount($where);
 
         if (empty($list)) {
             $list = array();
+            $res = array();
         }
+        foreach ($list as $v) {
+            $ware_where = ['=', 'sn', $v['storage_sn']];
+            $info = $ware->getOneRecord($ware_where, '', 'name');
+            if (empty($info)) {
+                $info['name'] = "";
+            }
+            $v['name'] = $info['name'];
+            $res[] = $v;
+        }
+
         $page_count = $count;
         if ($count > 0 && $page_size > 0) {
             $page_count = ceil($count / $page_size);
@@ -249,7 +268,7 @@ class StorageOutController extends BaseController
             'count' => $count,
             'pages'=> $pages,
             'page_count' => $page_count,
-            'data'=>$list,
+            'data'=>$res,
             'time_one'=>$time_one,
             'reason'=>$reason,
             'time_two'=>$time_two
@@ -302,11 +321,15 @@ class StorageOutController extends BaseController
     public function actionStock()
     {
         $id = RequestHelper::get('id', 0, 'intval');
+        $sn = RequestHelper::get('sn', '', 'trim');
         $sup_goods = new SupplierGood();
         $out_goods = new StorageOutGood();
         $where = [];
         if ($id != 0) {
             $where = ['=', 'good_id', $id];
+        }
+        if (!empty($sn) && $id != 0) {
+            $where = ['and',['=', 'good_id', $id], ['=', 'storage_sn', $sn]];
         }
         $info = $sup_goods->getOneInfo($where, 'good_id', '', 'sum(num) as stock');
         $out_info = $out_goods->getOneInfo($where, 'good_id', '', 'sum(num) as stock');
