348e683bd4ba17dfb454874c9bd4ae19cdbec359
李晨军 <lichenjun@iyangpin.com>
2015-08-29 15:09:41 +0800

    hetong

--- backend/modules/shop/controllers/ShopcontractController.php
+++ backend/modules/shop/controllers/ShopcontractController.php
@@ -266,7 +266,6 @@ class ShopcontractController extends BaseController
         $ShopManage_model_result = $ShopManage_model->insertOneData($ShopManageMsg);  //将经营信息插入到经营信息表里
         if ($ShopManage_model_result) {
             $this->_addOa($ShopContractMsg, $ShopManageMsg);
-            exit('success');
             return $this->success('商家合同添加操作成功！', 'index');
         } else {
             return $this->error('商家合同添加操作失败！', 'add');
