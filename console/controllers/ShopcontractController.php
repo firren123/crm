3e40df6d2ea18ab01f8a755dfa784f8b322f4862
李晨军 <lichenjun@iyangpin.com>
2015-09-07 11:15:56 +0800

    x

--- console/controllers/ShopcontractController.php
+++ console/controllers/ShopcontractController.php
@@ -42,19 +42,27 @@ class ShopcontractController extends Controller
     {
         $connection = \Yii::$app->db_oa;
         $time = date("Y-m-d H:i:s", strtotime("-1 hour"));
-        $sql = 'select GROUP_CONCAT(run_id) as run_ids from flow_run_prcs where DELIVER_TIME>"'.$time.'" and FLOW_PRCS =4 and PRCS_FLAG = 3';
+        $sql = 'select GROUP_CONCAT(run_id) as run_ids from flow_run_prcs where DELIVER_TIME>"' . $time . '" and FLOW_PRCS =4 and PRCS_FLAG = 3';
         $ret = $connection->createCommand($sql)->queryAll();
         if ($ret) {
             $run_ids = $ret[0]['run_ids'];
-            echo $sql = 'select data_93 as crm_id from flow_data_160 where run_id in (' . $run_ids . ')';
-            $crm_ids = $connection->createCommand($sql)->queryAll();
-            foreach ($crm_ids as $k => $v) {
-                $shopContractModel = new ShopContract();
-                $t = $shopContractModel->updateInfo(['status' => 1], ['id' => $v['crm_id']]);
-                if ($t) {
-                    echo $v['crm_id'] . "success\n";
+            if ($run_ids) {
+                $sql = 'select data_93 as crm_id from flow_data_160 where run_id in (' . $run_ids . ')';
+                $crm_ids = $connection->createCommand($sql)->queryAll();
+                foreach ($crm_ids as $k => $v) {
+                    $shopContractModel = new ShopContract();
+                    $t = $shopContractModel->updateInfo(['status' => 1], ['id' => $v['crm_id']]);
+                    if ($t) {
+                        echo $v['crm_id'] . "success\n";
+                    }else{
+                        echo $v['crm_id'] . "error\n";
+                    }
                 }
+            }else{
+                echo "empty";
             }
+        }else{
+            echo "sql error";
         }
     }
 }
