e975af87ef7f81bb1d5c4a1cbc1cd874d6e83b2f
孙松松 <sunsongsong@iyangpin.com>
2015-09-10 13:48:23 +0800

    代码编码修改

--- backend/models/social/ExChange.php
+++ backend/models/social/ExChange.php
@@ -1,28 +1,28 @@
 <?php
 /**
- * 下载页面
+ * 退换货订单表
  *
  * PHP Version 5
  *
  * @category  Crm
- * @package   Order.php
- * @author    liuwei <liuwei@iyangpin.com>
+ * @package   ExChange.php
+ * @author    sunsong <sunsongsong@iyangpin.com>
  * @time      2015/9/5 0005 上午 11:59
  * @copyright 2015 灵韬致胜（北京）科技发展有限公司
  * @license   http://www.i500m.com license
- * @link      liuwei@iyangpin.com
+ * @link      sunsongsong@iyangpin.com
  */
 
 namespace backend\models\social;
 
 /**
- * Order
+ * ExChange
  *
  * @category CRM
- * @package  Order
- * @author   liuwei <liuwei@iyangpin.com>
+ * @package  ExChange
+ * @author   sunsong <sunsongsong@iyangpin.com>
  * @license  http://www.i500m.com/ license
- * @link     liuwei@iyangpin.com
+ * @link     sunsongsong@iyangpin.com
  */
 class ExChange extends SocialBase
 {
--- backend/models/social/ExChangeChecked.php
+++ backend/models/social/ExChangeChecked.php
@@ -1,22 +1,22 @@
 <?php
 /**
- * 下载页面
+ * 审核数据表
  *
  * PHP Version 5
  *
  * @category  Crm
- * @package   Order.php
- * @author    liuwei <liuwei@iyangpin.com>
+ * @package   ExChangeChecked.php
+ * @author    sunsong <sunsongsong@iyangpin.com>
  * @time      2015/9/5 0005 上午 11:59
  * @copyright 2015 灵韬致胜（北京）科技发展有限公司
  * @license   http://www.i500m.com license
- * @link      liuwei@iyangpin.com
+ * @link      sunsongsong@iyangpin.com
  */
 
 namespace backend\models\social;
 
 /**
- * Order
+ * ExChangeChecked
  *
  * @category CRM
  * @package  Order
