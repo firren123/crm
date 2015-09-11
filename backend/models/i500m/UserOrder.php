cee152732ebd994f929d8f8529b80b82f8c3344f
李晨军 <lichenjun@iyangpin.com>
2015-09-11 14:53:17 +0800

    code 规范

--- backend/models/i500m/UserOrder.php
+++ backend/models/i500m/UserOrder.php
@@ -6,7 +6,7 @@
  * 文件介绍2
  *
  * @category  PHP
- * @package   admin
+ * @package   Admin
  * @filename  UserOrder.php
  * @author    lichenjun <lichenjun@iyangpin.com>
  * @copyright 2015 www.i500m.com
@@ -19,31 +19,36 @@
 
 namespace backend\models\i500m;
 
-
-class UserOrder extends I500Base{
+/**
+ * Class UserOrder
+ * @category  PHP
+ * @package   UserOrder
+ * @author    lichenjun <lichenjun@iyangpin.com>
+ * @copyright 2015 www
+ * @license   http://www.i500m.com/ i500m license
+ * @link      http://www.i500m.com/
+ */
+class UserOrder extends I500Base
+{
 
     /**
      * 数据库
-     *
      * @return string
      */
     public static function tableName()
     {
         return '{{%order}}';
     }
+
     /**
-     *
+     * 简介：
+     * @param string $order_sn order_sn
+     * @return array|null|\yii\db\ActiveRecord
      */
     public function UserDetailByOrderId($order_sn)
     {
         if (empty($order_sn)) return array();
-        $detail = $this::find()->where(['order_sn'=>$order_sn])->asArray()->one();
-
-
+        $detail = $this::find()->where(['order_sn' => $order_sn])->asArray()->one();
         return $detail;
     }
-
-
-
-
 }
--- backend/models/i500m/Warehouse.php
+++ backend/models/i500m/Warehouse.php
@@ -1,21 +1,30 @@
 <?php
 /**
  *  库房管理
- * @category  crm
+ * @category  Crm
  * @package   Warehouse.php
  * @author    youyong <youyong@iyangpin.com>
- * @time      2015/5/26 11:53 
- * @copyright 灵韬致胜（北京）科技发展有限公司
- * @license   http://www.i500m.com
+ * @time      2015/5/26 11:53
+ * @copyright 2015 灵韬致胜（北京）科技发展有限公司
+ * @license   i500m http://www.i500m.com
  * @link      youyong@iyangpin.com
  */
+
 namespace backend\models\i500m;
 
+/**
+ * Class Warehouse
+ * @category  PHP
+ * @package   Warehouse
+ * @author    lichenjun <lichenjun@iyangpin.com>
+ * @copyright 2015 www
+ * @license   http://www.i500m.com/ i500m license
+ * @link      http://www.i500m.com/
+ */
 class Warehouse extends I500Base
 {
     /**
      * 数据库
-     *
      * @return string
      */
     public static function tableName()
@@ -31,21 +40,21 @@ class Warehouse extends I500Base
     public function attributeLabels()
     {
         return array(
-            'sn' =>'库房编号',
+            'sn' => '库房编号',
             'name' => '库房名称',
             'username' => '库房联系人',
             'phone' => '联系电话',
-            'bc_id' =>'分公司ID',
+            'bc_id' => '分公司ID',
             'province_id' => '省ID',
-            'city_id'=>'市ID',
-            'district_id'=>'县ID',
-            'address'=>'详细地址',
-            'remarks'=>'仓库备注',
-            'status'=>'有效性',
+            'city_id' => '市ID',
+            'district_id' => '县ID',
+            'address' => '详细地址',
+            'remarks' => '仓库备注',
+            'status' => '有效性',
         );
     }
+
     /**
-     *
      * 简介：定义过滤规则
      * @author  lichenjun@iyangpin.com。
      * @return array
@@ -54,16 +63,15 @@ class Warehouse extends I500Base
     {
         return [
             //不可为空的字段
-            [['sn','name','username','phone','bc_id','province_id','city_id','district_id','address','remarks','status'],'required'],
-            ['phone','match','pattern'=>'/^1[0-9]{10}$/','message'=>'{attribute}格式输入不正确'],
-            ['sn','match','pattern'=>'/^[a-zA-Z0-9]{20}$/','message'=>'{attribute}是由20英文＋数字组成'],
+            [['sn', 'name', 'username', 'phone', 'bc_id', 'province_id', 'city_id', 'district_id', 'address', 'remarks', 'status'], 'required'],
+            ['phone', 'match', 'pattern' => '/^1[0-9]{10}$/', 'message' => '{attribute}格式输入不正确'],
+            ['sn', 'match', 'pattern' => '/^[a-zA-Z0-9]{20}$/', 'message' => '{attribute}是由20英文＋数字组成'],
         ];
     }
 
     /**
      * 库房删除
-     *
-     * @param: $id
+     * @param int $id ID
      * @return int
      * @throws \Exception
      */
@@ -74,7 +82,7 @@ class Warehouse extends I500Base
         } else {
             $list = $this->findOne($id);
             $result = $list->delete();
-            if ($result==true) {
+            if ($result == true) {
                 return 200;
             } else {
                 return 0;
@@ -85,19 +93,18 @@ class Warehouse extends I500Base
 
     /**
      * 库房名称是否存在
-     *
-     * @param: $name
-     * @param: NULL $id
-     * @return array|null|ActiveRecord
+     * @param string $name x
+     * @param NULL   $id   x
+     * @return array|null|\yii\db\ActiveRecord
      */
-    public function getDetailsByName($name, $id=NULL)
+    public function getDetailsByName($name, $id = NULL)
     {
         $list = array();
         if (!empty($name)) {
             if (empty($id)) {
-                $list = $this->find()->where("name="."'".$name."'")->asArray()->one();
+                $list = $this->find()->where("name=" . "'" . $name . "'")->asArray()->one();
             } else {
-                $list = $this->find()->where("name="."'".$name."' and id!=".$id)->asArray()->one();
+                $list = $this->find()->where("name=" . "'" . $name . "' and id!=" . $id)->asArray()->one();
             }
         }
         return $list;
