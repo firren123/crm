ccccc3b680a0b534570942c967997e9a750930a7
weitonghe <weitonghe@iyangpin.com>
2015-09-11 10:23:59 +0800

    code 规范

--- backend/models/i500m/User.php
+++ backend/models/i500m/User.php
@@ -6,9 +6,13 @@
  * 文件介绍2
  *
  * @category  PHP
+<<<<<<< HEAD
  * @package   Admin
+=======
+ * @package   CRM
+>>>>>>> crmwth
  * @filename  User.php
- * @author    lichenjun <lichenjun@iyangpin.com>
+ * @author    weitonghe <weitonghe@iyangpin.com>
  * @copyright 2015 www.i500m.com
  * @license   http://www.i500m.com/ i500m license
  * @datetime  15/4/20 下午2:50
@@ -17,6 +21,7 @@
  */
 namespace backend\models\i500m;
 
+<<<<<<< HEAD
 
 /**
  * Class User
@@ -29,9 +34,22 @@ namespace backend\models\i500m;
  */
 class User extends I500Base
 {
+=======
+/**
+ * 用户表
+ *
+ * @category MODEL
+ * @package  User
+ * @author   weitonghe <weitonghe@iyangpin.com>
+ * @license  http://www.i500m.com/ license
+ * @link     weitonghe@iyangpin.com
+ */
+class User extends I500Base
+{
+
+>>>>>>> crmwth
     /**
-     * 数据库
-     *user 表
+     * 设置表名称
      * @return string
      */
     public static function tableName()
@@ -46,16 +64,25 @@ class User extends I500Base
     public function rules()
     {
         return [
+<<<<<<< HEAD
             //不可为空的字段
             [['password'], 'required', 'message' => '密码不能为空！'],
             [['email'], 'required', 'message' => '电子邮箱不能为空！'],
             [['mobile'], 'required', 'message' => '手机号不能为空！'],
             [['status'], 'required', 'message' => '状态不能为空！'],
             [['balance'], 'required', 'message' => '余额不能为空！'],
+=======
+            [['password'],'required','message' => '密码不能为空！'],
+            [['email'],'required','message' => '电子邮箱不能为空！'],
+            [['mobile'],'required','message' => '手机号不能为空！'],
+            [['status'],'required','message' => '状态不能为空！'],
+            [['balance'],'required','message' => '余额不能为空！'],
+>>>>>>> crmwth
         ];
     }
 
     /**
+<<<<<<< HEAD
      * 简介：获得总的记录条数
      * @param $where
      * @return int|string
@@ -64,10 +91,21 @@ class User extends I500Base
     {
         $listcount = User::find()->andFilterWhere($where)->count();
         return $listcount;
+=======
+     * 获得总的记录条数
+     * @param Array $where 查询条件
+     * @return array|int|string
+     */
+    public function totalNum($where)
+    {
+        $listCount = User::find()->andFilterWhere($where)->count();
+        return $listCount;
+>>>>>>> crmwth
     }
 
     /**
      * 根据条件查询记录 并分页
+<<<<<<< HEAD
      * @param array  $where  X
      * @param string $fields X
      * @param string $order  X
@@ -78,6 +116,17 @@ class User extends I500Base
     public function getAllUser($where = array(), $fields = '*', $order = '', $page = 1, $size = 2)
     {
         $allGoods = [];
+=======
+     * @param array  $where  条件
+     * @param string $fields 字段
+     * @param string $order  排序
+     * @param int    $page   页数
+     * @param int    $size   每页记录数
+     * @return array|\yii\db\ActiveRecord[]
+     */
+    public function getAllUser($where=array() , $fields='*' , $order='' , $page = 1 , $size = 2)
+    {
+>>>>>>> crmwth
         $allGoods = $this->find()
             ->where($where)
             ->select($fields)
@@ -91,6 +140,7 @@ class User extends I500Base
 
     /**
      * 获得一个人的信息
+<<<<<<< HEAD
      * @param array  $where X
      * @param string $field X
      * @return array|\yii\db\ActiveRecord[]
@@ -99,20 +149,37 @@ class User extends I500Base
     {
         $oneuser = [];
         $oneuser = $this->find()
+=======
+     * @param array  $where 条件
+     * @param string $field 字段
+     * @return array|null|\yii\db\ActiveRecord
+     */
+    public function getOneuser($where = array() , $field = '*')
+    {
+        $oneUser = $this->find()
+>>>>>>> crmwth
             ->where($where)
             ->select($field)
             ->asArray()
             ->one();
-        return $oneuser;
+        return $oneUser;
     }
 
     /**
      * 修改一个人的信息
+<<<<<<< HEAD
      * @param int   $id  x
      * @param array $msg x
      * @return bool
      */
     public function editUserMsg($id, $msg = [])
+=======
+     * @param int   $id  ID
+     * @param array $msg Data
+     * @return bool
+     */
+    public function editUserMsg($id,$msg=[])
+>>>>>>> crmwth
     {
         $user = $this->findOne($id);
 
@@ -121,39 +188,64 @@ class User extends I500Base
         $user->status = $msg['status'];
         $user->balance = $msg['balance'];
 
-        $result = $user->save();  // 等同于 $supplier->update();
+        $result = $user->save();
         return $result;
     }
 
     /**
      * 重置密码
+<<<<<<< HEAD
      * @param int    $id  x
      * @param string $psw x
      * @return bool
      */
     public function editUserPswMsg($id, $psw)
+=======
+     * @param int    $id  ID
+     * @param string $psw Password
+     * @return bool
+     */
+    public function editUserPswMsg($id,$psw)
+>>>>>>> crmwth
     {
         $user = $this->findOne($id);
         $salt = rand(100000, 999999);
         $user->salt = $salt;
+<<<<<<< HEAD
         $user->password = md5($salt . md5($psw));
         $result = $user->save();  // 等同于 $supplier->update();
+=======
+        $user->password = md5($salt.md5($psw));
+        $result = $user->save();
+>>>>>>> crmwth
         return $result;
     }
 
     /**
+<<<<<<< HEAD
      * 简介：冻结/解锁用户
      * @param int $id x
+=======
+     * 冻结/解锁用户
+     * @param int $id ID
+>>>>>>> crmwth
      * @return bool
      */
     public function lock($id)
     {
         $user = $this->findOne($id);
         if (!empty($user)) {
+<<<<<<< HEAD
             if ($user['status'] == 1) {
                 $user['status'] = 2;
             } else {
                 $user['status'] = 1;
+=======
+            if ($user['status']==1) {
+                $user['status']=2;
+            } else {
+                $user['status']=1;
+>>>>>>> crmwth
             }
             $result = $user->save();
             return $result;
@@ -164,6 +256,7 @@ class User extends I500Base
 
     /**
      * 全部锁定
+<<<<<<< HEAD
      * @param array $arrid x
      * @return bool
      */
@@ -182,6 +275,22 @@ class User extends I500Base
                 $userid->status = 1;
                 $result = $userid->save();
                 if ($result != 1) {
+=======
+     * @param array $arrId ArrID
+     * @return bool
+     */
+    public function alllock($arrId = array())
+    {
+        foreach ($arrId as $k => $v) {
+            if (empty($v)) {
+                continue;
+            }
+            $userId = User :: findOne($v);
+            if (!empty($userId)) {
+                $userId->status = 1;
+                $result = $userId->save();
+                if ($result!=1) {
+>>>>>>> crmwth
                     return false;
                 }
             }
@@ -191,6 +300,7 @@ class User extends I500Base
 
     /**
      * 全部激活
+<<<<<<< HEAD
      * @param array $arrid x
      * @return bool
      */
@@ -207,6 +317,22 @@ class User extends I500Base
                 $userid->status = 2;
                 $result = $userid->save();
                 if ($result != 1) {
+=======
+     * @param array $arrId ArrID
+     * @return bool
+     */
+    public function allunlock($arrId = array())
+    {
+        foreach ($arrId as $k => $v) {
+            if (empty($v)) {
+                continue;
+            }
+            $userId = User :: findOne($v);
+            if (!empty($userId)) {
+                $userId->status = 2;
+                $result = $userId->save();
+                if ($result!=1) {
+>>>>>>> crmwth
                     return false;
                 }
             }
@@ -214,6 +340,7 @@ class User extends I500Base
         return true;
     }
 
+<<<<<<< HEAD
     /**
      * 简介：
      * @return bool
@@ -245,3 +372,6 @@ class User extends I500Base
         return true;
     }
 }
+=======
+}
+>>>>>>> crmwth
--- backend/modules/user/controllers/UserController.php
+++ backend/modules/user/controllers/UserController.php
@@ -3,8 +3,8 @@
  * 用户管理
  *
  * PHP Version 5
- * 用户想过操作
  *
+<<<<<<< HEAD
  * @category  CRM
  * @package   UserController
  * @author    liubaocheng <liubaocheng@iyangpin.com>
@@ -12,6 +12,15 @@
  * @copyright 2015 灵韬致胜（北京）科技发展有限公司
  * @license   2015 http://www.i500m.com
  * @link      liubaocheng@iyangpin.com
+=======
+ * @category  PHP
+ * @package   CRM
+ * @author    weitonghe <weitonghe@iyangpin.com>
+ * @time      15/4/20 下午2:50
+ * @copyright 2015 灵韬致胜（北京）科技发展有限公司
+ * @license   http://www.i500m.com license
+ * @link      weitonghe@iyangpin.com
+>>>>>>> crmwth
  */
 namespace backend\modules\user\controllers;
 
@@ -22,6 +31,7 @@ use common\helpers\RequestHelper;
 use yii\data\Pagination;
 
 /**
+<<<<<<< HEAD
  * Class UserController
  * @category  PHP
  * @package   UserController
@@ -29,15 +39,29 @@ use yii\data\Pagination;
  * @copyright 2015 www
  * @license   http://www.i500m.com/ i500m license
  * @link      http://www.i500m.com/
+=======
+ * 用户管理
+ *
+ * @category PHP
+ * @package  CRM
+ * @author   weitonghe <weitonghe@iyangpin.com>
+ * @license  http://www.i500m.com/ license
+ * @link     weitonghe@iyangpin.com
+>>>>>>> crmwth
  */
 class UserController extends BaseController
 {
     /**
+<<<<<<< HEAD
      * 简介：用户列表
+=======
+     * 用户列表
+>>>>>>> crmwth
      * @return string
      */
     public function actionIndex()
     {
+<<<<<<< HEAD
         $list = new User();
         //分页
         $page = RequestHelper::get('page', 1, 'intval');//获得当前的页数
@@ -63,13 +87,33 @@ class UserController extends BaseController
 
     /**
      * 简介：显示个人详情页
+=======
+        $list=new User();
+        $page = RequestHelper::get('page', 1, 'intval');//获得当前的页数
+        $pageSize = 12;                                 //设置每页显示的记录条数
+        // 查询条件
+        $username = RequestHelper::get('username', '', 'htmlspecialchars');
+        $cond   = ['like','username',$username];
+        $total  = $list->totalNum($cond);//获得总记录条数
+        $pages  = new Pagination(['totalCount' => $total, 'pageSize' => $pageSize]);
+        $fields = '*';
+        $order  = '';
+        $allUsers = $list->getAllUser($cond, $fields, $order, $page, $pageSize);
+        return $this->render('index', array('list' => $allUsers,'pages' => $pages,'model' => $list));
+    }
+    /**
+     * 显示个人详情页
+>>>>>>> crmwth
      * @return string
      */
     public function actionShowoneuser()
     {
         $list = new User();
         $id = RequestHelper::get('id');
+<<<<<<< HEAD
         $cond = array();
+=======
+>>>>>>> crmwth
         $cond['id'] = $id;
         $fields = '*';
         $allUsers = $list->getOneuser($cond, $fields);
@@ -77,13 +121,18 @@ class UserController extends BaseController
     }
 
     /**
+<<<<<<< HEAD
      * 简介：重置一个用户的密码  显示个人修改页
+=======
+     * 重置一个用户的密码/显示个人修改页
+>>>>>>> crmwth
      * @return string
      */
     public function actionEditoneuser()
     {
         $list = new User();
         $id = RequestHelper::get('id');
+<<<<<<< HEAD
         $cond = array();
         $cond['id'] = $id;
         $fields = '*';
@@ -172,23 +221,118 @@ class UserController extends BaseController
             return $this->success('锁定成功！', 'index');
         } else {
             return $this->error('锁定失败！(用户信息需要完善)', 'index');
+=======
+        $cond['id'] = $id;
+        $fields = '*';
+        $allUsers=$list->getOneuser($cond, $fields);
+        $Psw = RequestHelper::get('psw');
+        if (empty($Psw)) {
+            return $this->render('editoneuser', array('list' => $allUsers,'model' => $list));
+        }
+        //记录日志  刘伟
+        $content = "管理员：".\Yii::$app->user->identity->username.',修改了:'.$allUsers['username'].'('.$id.')'.' 的密码';
+        $log_model = new Log();
+        $log_model->recordLog($content, 5);
+        return $this->render('editoneuserpsw', array('list' => $allUsers,'model' => $list));
+    }
+    /**
+     * 修改个人信息
+     * @return string
+     */
+    public function actionEditoneusermsg()
+    {
+        $model = new User();
+        $editId  = RequestHelper::post('editid');
+        $editPwd = RequestHelper::post('password');
+        $editMsg = array(
+            'email'=>RequestHelper::post('email'),
+            'mobile'=>RequestHelper::post('mobile'),
+            'status'=>RequestHelper::post('status'),
+            'balance'=>RequestHelper::post('balance')
+        );
+        if (!empty($editPwd)) {
+            $result=$model->editUserPswMsg($editId, $editPwd);
+        } else {
+            $item = $model->getInfo(['id'=>$editId]);
+            $array = array_diff($editMsg, $item);
+            //记录日志  刘伟
+            if ($array or $editMsg['status']!=$item['status']) {
+                $content = "管理员：".\Yii::$app->user->identity->username.',修改了:'.$item['username'].'('.$editId.')';
+                if (!empty($array['email'])) {
+                    $content .=  ',邮箱:'.$editMsg['email'];
+                }
+                if (!empty($array['mobile'])) {
+                    $content .=  ',手机号:'.$editMsg['mobile'];
+                }
+                if ($editMsg['status']!=$item['status']) {
+                    $status = $editMsg['status']==1 ? '冻结' : '激活';
+                    $content .=  ',状态id:'.$editMsg['status'].' 状态值:'.$status;
+                }
+                if (!empty($array['balance'])) {
+                    $content .=  ',账户余额:'.$editMsg['balance'];
+                }
+                $log_model = new Log();
+                $log_model->recordLog($content, 5);
+            }
+            $result = $model->editUserMsg($editId, $editMsg);
+        }
+        if ($result) {
+            return $this->success('修改成功！', 'index');
+        } else {
+            return $this->error('修改失败！', 'index');
+>>>>>>> crmwth
         }
     }
 
     /**
+<<<<<<< HEAD
      * 简介：锁定大批用户
      * @author  lichenjun@iyangpin.com。
      * @return string
      */
     public function actionAlllock()
+=======
+     * 锁定一个用户
+     * @return string
+     */
+    public function actionLockoneuser()
+>>>>>>> crmwth
     {
         $list = new User();
-        $arrid = array();
+        $id   = RequestHelper::get('id');
+        $item = $list->getInfo(['id'=>$id]);
+        $oneUser = $list->lock($id);
+        //判断是否更改成功
+        if ($oneUser) {
+            //记录日志  刘伟
+            $content = "管理员：".\Yii::$app->user->identity->username.',把:'.$item['username'].'('.$id.')'.' 的状态修改为:锁定';
+            $log_model = new Log();
+            $log_model->recordLog($content, 5);
+            return $this->success('锁定成功！', 'index');
+        } else {
+            return $this->error('锁定失败！(用户信息需要完善)', 'index');
+        }
+    }
+
+    /**
+     * 锁定大批用户
+     * @return string
+     */
+    public function actionAlllock()
+    {
+        $list  = new User();
         $id = RequestHelper::get('arrid');
+<<<<<<< HEAD
         $arrid = explode(',', $id);
         $arruser = $list->alllock($arrid);
         //判断是否更改成功
         if ($arruser) {
+=======
+        $arrId = explode(',', $id);
+        $arrUser = $list->alllock($arrId);
+        //判断是否更改成功
+        if ($arrUser) {
+>>>>>>> crmwth
             //记录日志  刘伟
             $content = "管理员：" . \Yii::$app->user->identity->username . ',把id为:(' . $id . ')' . ' 的状态修改为:锁定';
             $log_model = new Log();
@@ -198,14 +342,20 @@ class UserController extends BaseController
             return $this->error('全部锁定失败！(用户信息需要完善)', 'index');
         }
     }
+<<<<<<< HEAD
 
     /**
      * 简介：解锁一个用户
+=======
+    /**
+     * 解锁一个用户
+>>>>>>> crmwth
      * @return string
      */
     public function actionUnlockoneuser()
     {
         $list = new User();
+<<<<<<< HEAD
         $id = RequestHelper::get('id');
         $item = $list->getInfo(['id' => $id]);
         $oneuser = $list->lock($id);
@@ -213,6 +363,13 @@ class UserController extends BaseController
         //exit;
         //判断是否更改成功
         if ($oneuser) {
+=======
+        $id   = RequestHelper::get('id');
+        $item = $list->getInfo(['id'=>$id]);
+        $oneUser = $list->lock($id);
+        //判断是否更改成功
+        if ($oneUser) {
+>>>>>>> crmwth
             //记录日志  刘伟
             $content = "管理员：" . \Yii::$app->user->identity->username . ',把:' . $item['username'] . '(' . $id . ')' . ' 的状态修改为:激活';
             $log_model = new Log();
@@ -221,22 +378,35 @@ class UserController extends BaseController
         } else {
             return $this->error('解锁失败！', 'index');
         }
+<<<<<<< HEAD
 
     }
 
     /**
      * 简介：解锁大批用户
+=======
+    }
+
+    /**
+     * 解锁大批用户
+>>>>>>> crmwth
      * @return string
      */
     public function actionAllunlock()
     {
         $list = new User();
-        $arrid = array();
         $id = RequestHelper::get('arrid');
+<<<<<<< HEAD
         $arrid = explode(',', $id);
         $arruser = $list->allunlock($arrid);
         //判断是否更改成功
         if ($arruser) {
+=======
+        $arrId = explode(',', $id);
+        $arrUser = $list->allunlock($arrId);
+        //判断是否更改成功
+        if ($arrUser) {
+>>>>>>> crmwth
             //记录日志  刘伟
             $content = "管理员：" . \Yii::$app->user->identity->username . ',把id为:(' . $id . ')' . ' 的状态修改为:激活';
             $log_model = new Log();
@@ -246,6 +416,7 @@ class UserController extends BaseController
             return $this->error('全部解锁失败！', 'index');
         }
     }
+<<<<<<< HEAD
 
     /**
      * 简介：
@@ -257,3 +428,6 @@ class UserController extends BaseController
         $arruser = $list->showdatatime();
     }
 }
+=======
+}
+>>>>>>> crmwth
