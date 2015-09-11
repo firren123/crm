075e5633a6bef6e29ff0db57cbf58bddd3de0f86
李晨军 <lichenjun@iyangpin.com>
2015-09-11 15:52:47 +0800

    code 规范

--- common/helpers/CurlHelper.php
+++ common/helpers/CurlHelper.php
@@ -5,19 +5,28 @@
  * PHP Version 5
  * 可写多行的文件相关说明
  *
- * @category  Wap   
- * @package   Member     (这里写模块名)
+ * @category  Wap
+ * @package   Member
  * @author    liubaocheng <liubaocheng@iyangpin.com>
- * @time      15/3/20 上午11:14 
- * @copyright 灵韬致胜（北京）科技发展有限公司
- * @license   http://www.i500m.com
+ * @time      15/3/20 上午11:14
+ * @copyright 2015 灵韬致胜（北京）科技发展有限公司
+ * @license   license http://www.i500m.com
  * @link      liubaocheng@iyangpin.com
  */
 
 namespace common\helpers;
 
-
-class CurlHelper extends BaseCurlHelps{
+/**
+ * Class CurlHelper
+ * @category  PHP
+ * @package   CurlHelper
+ * @author    lichenjun <lichenjun@iyangpin.com>
+ * @copyright 2015 www
+ * @license   http://www.i500m.com/ i500m license
+ * @link      http://www.i500m.com/
+ */
+class CurlHelper extends BaseCurlHelps
+{
 
 
 }
