1840c6555e03c390beaa3d95dfdab607ac3730ae
李晨军 <lichenjun@iyangpin.com>
2015-09-14 13:25:26 +0800

    code 规范

--- backend/modules/goods/controllers/AttributeController.php
+++ backend/modules/goods/controllers/AttributeController.php
@@ -7,7 +7,7 @@
  * @category  I500M
  * @package   Member
  * @author    renyineng <renyineng@iyangpin.com>
- * @time      15/4/17 上午10:36 
+ * @time      15/4/17 上午10:36
  * @copyright 2015 灵韬致胜（北京）科技发展有限公司
  * @license   http://www.i500m.com license
  * @link      renyineng@iyangpin.com
@@ -69,7 +69,7 @@ class AttributeController extends BaseController
                 $content = "管理员：".\Yii::$app->user->identity->username.",添加了后台显示名称为:".$data['Attribute']['admin_name'].",添加了前台显示名称为:".$data['Attribute']['attr_name']." 的属性";
                 $log_model = new Log();
                 $log_model->recordLog($content);
-                return $this->success('保存成功','/goods/attribute');
+                return $this->success('保存成功', '/goods/attribute');
             } else {
                 return $this->error('保存失败');
             }
@@ -96,9 +96,6 @@ class AttributeController extends BaseController
     {
         $log_model = new Log();
         $attr_id = RequestHelper::get('id',0,'intval');
-//        if(empty($attr_id)) {
-//            return $this->error('错误的参数');
-//        }
         $model = new Attribute();
         $show = $model->getInfo(['id'=>$attr_id], false);
         $list = $model->getInfo(['id'=>$attr_id]);
@@ -109,27 +106,27 @@ class AttributeController extends BaseController
             $data = Yii::$app->request->post();
             if (isset($data['Attribute'])) {
                 //修改属性
-                $re = $model->updateAttribute($show ,$data['Attribute']);
+                $re = $model->updateAttribute($show, $data['Attribute']);
                 //日志
                 $array = array_diff($data['Attribute'], $list);
-                    if (!empty($array)) {
-                        $content = "管理员：" . \Yii::$app->user->identity->username . ",修改了属性id为:" . $attr_id . " 属性的";
-                        if (!empty($array['admin_name'])) {
-                            $content .= " 后台显示名称,修改为了:" . $array['admin_name'];
-                        }
-                        if (!empty($array['attr_name'])) {
-                            $content .= " 前台显示名称,修改为了:" . $array['attr_name'];
-                        }
-                        if (!empty($array['weight'])) {
-                            $content .= " 权重,修改为了:" . $array['weight'];
-                        }
-                        if (!empty($array['is_search'])) {
-                            $status_name = $array['is_search'] == 1 ? '是' : '否';
-                            $content .= " 是否检索属性,修改为了:" . $status_name;
-                        }
-
-                        $log_model->recordLog($content);
+                if (!empty($array)) {
+                    $content = "管理员：" . \Yii::$app->user->identity->username . ",修改了属性id为:" . $attr_id . " 属性的";
+                    if (!empty($array['admin_name'])) {
+                        $content .= " 后台显示名称,修改为了:" . $array['admin_name'];
                     }
+                    if (!empty($array['attr_name'])) {
+                        $content .= " 前台显示名称,修改为了:" . $array['attr_name'];
+                    }
+                    if (!empty($array['weight'])) {
+                        $content .= " 权重,修改为了:" . $array['weight'];
+                    }
+                    if (!empty($array['is_search'])) {
+                        $status_name = $array['is_search'] == 1 ? '是' : '否';
+                        $content .= " 是否检索属性,修改为了:" . $status_name;
+                    }
+
+                    $log_model->recordLog($content);
+                }
                 //$re = 1;
                 if (isset($data['AttributeValue'])) {
                     $res = $model_value->updateValue($data['AttributeValue']);
@@ -167,7 +164,6 @@ class AttributeController extends BaseController
                 'model_value'=>$model_value,
                 'value_list'=>$value_list,
             ];
-           // var_dump($value_list);exit();
             return $this->render('edit', $params);
             //return $this->render('add', );
         }
@@ -179,7 +175,7 @@ class AttributeController extends BaseController
      */
     public function actionDel()
     {
-        $id = RequestHelper::get('id',0,'intval');
+        $id = RequestHelper::get('id', 0, 'intval');
         $model = new Attribute();
         $list = $model->getInfo(['id'=>$id]);
         $re = $model->delAttribute($id);
@@ -199,11 +195,11 @@ class AttributeController extends BaseController
      */
     public function actionDelValue()
     {
-        $id = RequestHelper::get('id',0,'intval');
+        $id = RequestHelper::get('id', 0, 'intval');
         //$model = new AttributeValue();
         $attr_value = AttributeValue::findOne($id);
         if (empty($attr_value)) {
-            return $this->ajaxReturn(0,'删除失败,此记录不存在');
+            return $this->ajaxReturn(0, '删除失败,此记录不存在');
         } else {
             $re = $attr_value->delete();
             if ($re) {
@@ -211,9 +207,9 @@ class AttributeController extends BaseController
                 $content = "管理员：".\Yii::$app->user->identity->username.",删除了属性id为:".$attr_value['attr_id'].",id为:".$id.",属性值为:".$attr_value['attr_value']." 的商品属性值";
                 $log_model = new Log();
                 $log_model->recordLog($content);
-                return $this->ajaxReturn(200,'删除成功');
+                return $this->ajaxReturn(200, '删除成功');
             } else {
-                return $this->ajaxReturn(0,'删除失败');
+                return $this->ajaxReturn(0, '删除失败');
             }
         }
 
