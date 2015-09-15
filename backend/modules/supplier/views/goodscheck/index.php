316175156ceb3a6a96f8855a78662864bef3b453
zhengyu <zhengyu@iyangpin.com>
2015-09-15 16:04:16 +0800

    供应商商品审核，列表，筛选

--- backend/modules/supplier/views/goodscheck/index.php
+++ backend/modules/supplier/views/goodscheck/index.php
@@ -61,7 +61,7 @@ $this->title = "商家列表";
 
 <div class="zcss_query_aera">
 
-    <select class="zjs_select_province_open">
+    <select class="zjs_select_cate">
         <option value="0">全部分类</option>
         <?php foreach($arr_all_cate as $a_cate){ ?>
             <option value="<?php echo $a_cate['id']; ?>"<?php if($arr_select_param['category_id']==$a_cate['id']){echo " selected=\"selected\" ";} ?>><?php echo $a_cate['name']; ?></option>
@@ -70,10 +70,10 @@ $this->title = "商家列表";
 
 
     <span class="zcss_span1">商品名称:</span>
-    <input type="text" class="zjs_query_id" style="width:50px;" value="<?php if($arr_select_param['like_goods_name']!=''){echo $arr_select_param['like_goods_name'];} ?>" />
+    <input type="text" class="zjs_query_goods_name" style="width:150px;" value="<?php if($arr_select_param['like_goods_name']!=''){echo $arr_select_param['like_goods_name'];} ?>" />
 
     <span class="zcss_span1">条形码:</span>
-    <input type="text" class="zjs_query_shop_name" style="width:80px;" value="<?php if($arr_select_param['bar_code']!=''){echo $arr_select_param['bar_code'];} ?>" />
+    <input type="text" class="zjs_query_bar_code" style="width:150px;" value="<?php if($arr_select_param['bar_code']!=''){echo $arr_select_param['bar_code'];} ?>" />
 
 
     <input type="button" value="查询" class="zjs_btn_query" style="width:80px;margin:0 0 0 30px;" />
@@ -139,128 +139,23 @@ $this->title = "商家列表";
 
 
 <script type="text/javascript">
-    $(function()
+$(function()
+{
+    $(".zjs_btn_query").click(function()
     {
-        $(".zjs_btn_query").click(function()
-        {
-            get_list();
-        });
-
-        $(".zjs_query_business_name").focus(function()
-        {
-            layer.tips(
-                "可输入姓名或id",
-                ".zjs_query_business_name",
-                {
-                    tips: [1, '#0088cc'],
-                    time: 5000
-                }
-            );
-        });
-
-
-        $(".zjs_query_business_name").keyup(function()
-        {
-            get_business();
-        });
-
-        $(document).on("change",".zjs_select_business",function()
-        {
-            var value=parseInt($(".zjs_select_business").val());
-            if(value==0){
-                $(".zjs_query_business_name").val('');
-                return;
-            }else{
-                var str=$(".zjs_select_business option:selected").html();
-                $(".zjs_query_business_name").val(str);
-                $(".zjs_select_business").hide();
-            }
-        });
-
-
+        get_list();
     });
 
-    function get_list()
-    {
-        //var status=$(".zjs_status").val();
-        var id=$(".zjs_query_id").val();
-        var province=$(".zjs_select_province_open").val();
-        var city=$(".zjs_select_city").val();
-        var district=$(".zjs_select_district").val();
-        var shop_name=$(".zjs_query_shop_name").val();
-        var contact_name=$(".zjs_query_contact_name").val();
-        var business_id=$(".zjs_select_business").val();
-        var business=$(".zjs_query_business_name").val();
-        if(business==''){
-            business_id=0;
-        }
-        if (business!='' && (business_id==0 || business_id=='')) {
-            layer.msg('请在下拉列表中选择业务员或重新输入');
-            //$(".zjs_query_business_name").val('');
-            return;
-        }
+});
 
+function get_list()
+{
+    var cate_id=$(".zjs_select_cate").val();
+    var goods_name=$(".zjs_query_goods_name").val();
+    var bar_code=$(".zjs_query_bar_code").val();
 
-        var url="/shop/shop/index?id="+id+"&province="+province+"&city="+city+"&district="+district+"&like_shop_name="+encodeURIComponent(shop_name)+"&like_contact_name="+encodeURIComponent(contact_name)+"&business_id="+business_id;
-        //alert(url);
-        //console.log(url);
-        window.location.href=url;
-    }
-
-    function get_business()
-    {
-        var kw=$(".zjs_query_business_name").val();
-        if(kw==''){
-            return;
-        }
-        if(kw.length>10){
-            return;
-        }
-        if ((/^[A-Za-z]+$/.test(kw))) {
-            return;
-        }
-
-        var x=$(".zjs_query_business_name").offset().left;
-        var y=$(".zjs_query_business_name").offset().top + $(".zjs_query_business_name").height()+6;
-        $(".zjs_select_business").html("<option value='0'>获取中...</option>").show().offset({"top":y,"left":x});
-        $.ajax({
-            "type":"post",
-            "url": "/shop/shop/ajax",
-            "dataType":"json",
-            "data":{
-                "act":"getsalesman",
-                "kw":kw
-            },
-            "success":function(obj){
-                //console.log("success");
-                if(obj.code=='200'){
-                    if(obj.data.data.length>0){
-                        var len=obj.data.data.length;
-                        var html='';
-                        var data=obj.data.data;
-                        html+='<option value="0">请选择</option>';
-                        for(var i=0;i<len;i++){
-                            //html+='<option value="'+data[i].id+'">'+data[i].name+'-'+data[i].id+'</option>';
-                            html+='<option value="'+data[i].id+'">'+data[i].name+'</option>';
-                        }
-                        var x=$(".zjs_query_business_name").offset().left;
-                        var y=$(".zjs_query_business_name").offset().top + $(".zjs_query_business_name").height()+6;
-                        $(".zjs_select_business").html(html).show().offset({"top":y,"left":x});
-                    }else{
-                        $(".zjs_select_business").html("<option value='0'>无数据</option>");
-                    }
-                }else{
-                    $(".zjs_select_business").html("<option value='0'>查询失败</option>");
-                }
-            },
-            "error":function(){
-                //console.log("error");
-                $(".zjs_select_business").html("<option value='0'>查询失败</option>");
-            },
-            "complete":function(){
-                //console.log("complete");
-            }
-        });
-    }
+    var url="/supplier/goodscheck/index?cate_id="+cate_id+"&bar_code="+encodeURIComponent(bar_code)+"&like_goods_name="+encodeURIComponent(goods_name);
+    window.location.href=url;
+}
 
 </script>
