af0b64ed13b315f10d8e673d7fa039d13f2532c4
刘伟 <liuwei@iyangpin.com>
2015-09-21 17:43:47 +0800

    报表

--- backend/models/social/Order.php
+++ backend/models/social/Order.php
@@ -35,4 +35,73 @@ class Order extends SocialBase
     {
         return "{{%i500_order}}";
     }
+    /**
+     * 列表
+     * @author  lichenjun@iyangpin.com。
+     * @param array  $cond      条件1
+     * @param string $field     字段
+     * @param string $order     排序
+     * @param string $and_where 条件2
+     * @return array|\yii\db\ActiveRecord[]
+     */
+    public function getList($cond = array(), $field = '*', $order = '', $and_where = '', $where = '')
+    {
+        $list = [];
+        if ($cond || $and_where || $where) {
+            $list = $this->find()
+                ->select($field)
+                ->where($cond)
+                ->andWhere($where)
+                ->andWhere($and_where)
+                ->orderBy($order)
+                ->asArray()
+                ->all();
+        }
+        return $list;
+    }
+    /**
+     * 带分页的列表
+     * @author  liuwei@iyangpin.com。
+     * @param array  $cond      条件1
+     * @param string $field     字段
+     * @param string $order     排序
+     * @param int    $page      分页默认1
+     * @param int    $size      每页数量默认10
+     * @param string $and_where 条件2
+     * @param string $where     条件3
+     * @return array|\yii\db\ActiveRecord[]
+     */
+    public function getPageLists($cond = array(), $field = '*', $order = '', $page = 1, $size = 10, $and_where = '', $where = '')
+    {
+        $list = [];
+        if ($cond || $and_where || $where) {
+            $list = $this->find()
+                ->select($field)
+                ->where($cond)
+                ->andWhere($and_where)
+                ->andWhere($where)
+                ->orderBy($order)
+                ->offset(($page-1) * $size)
+                ->limit($size)
+                ->asArray()
+                ->all();
+        }
+        return $list;
+    }
+    /**
+     * 简介：获取总数
+     * @author  lichenjun@iyangpin.com。
+     * @param array  $cond      条件一
+     * @param string $and_where 条件二
+     * @param string $and       条件三
+     * @return int|string
+     */
+    public function getCounts($cond = array(), $and_where = '', $where = '')
+    {
+        $num = 0;
+        if ($cond || $and_where || $where) {
+            $num = $this->find()->where($cond)->andWhere($and_where)->andWhere($where)->count();
+        }
+        return $num;
+    }
 }
