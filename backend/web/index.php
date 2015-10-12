bc4e159265b30214880e42092455b961aea04a40
李晨军 <lichenjun@iyangpin.com>
2015-10-12 12:25:43 +0800

    test

--- backend/web/index.php
+++ backend/web/index.php
@@ -7,24 +7,17 @@ require(__DIR__ . '/../../vendor/yiisoft/yii2/Yii.php');
 require(__DIR__ . '/../../common/config/bootstrap.php');
 require(__DIR__ . '/../config/bootstrap.php');
 
-//$intRemoteIp = ip2long('220.194.44.25');
 $intRemoteIp = ip2long($_SERVER['SERVER_ADDR']);
-var_dump($intRemoteIp);
-var_dump(ip2long('192.168.33.0'));
-var_dump(ip2long('192.168.33.255'));
 if (($intRemoteIp - ip2long('192.168.33.0')) * ($intRemoteIp -ip2long('192.168.33.255')) > 0) {
-    echo '测试';
     $config = array_merge(
         require(__DIR__ . '/../../common/config/main.php'),
         require(__DIR__ . '/../config/main.php')
     );
 } else {
-    echo '本地';
     $config = array_merge(
         require(__DIR__ . '/../../common/config/main-local.php'),
         require(__DIR__ . '/../config/main-local.php')
     );
 }
-exit;
 $application = new yii\web\Application($config);
 $application->run();
